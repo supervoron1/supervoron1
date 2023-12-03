8d82ab964c3d8b63eb2903e690696b7d80070252
raven <vorontsof@mail.ru>
2023-12-04 01:24:42 +0400

    abot me info added :hammer:

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,33 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Текст писать тут
-        </p>
+        <img src={logo} className="App-logo" alt="logo"/>
+        <div>
+          <h3>Цели</h3>
+          <div>Структурировать знания по ReactJS</div>
+          <div>Научиться использовать продвинутые возможности Redux и Redux-Saga</div>
+          <div>Изучить Typescript</div>
+        </div>
+        <div>
+          <h3>Какими технологиями хочу овладеть</h3>
+          <div>ReactJS</div>
+          <div>Typescript</div>
+          <div>Redux + Redux-Saga </div>
+          <div>Jest</div>
+        </div>
+        <div>
+          <h3>Какими технологиями уже владею</h3>
+          <div>HTML, CSS</div>
+          <div>Javascript, VueJs</div>
+          <div>PHP, SQL</div>
+        </div>
+        <div>
+          <h3>О себе</h3>
+          <div>Меня зовут Дмитрий, в ИТ почти 5 лет.</div>
+          <div>Работал как на фронте, так и на бэке (PHP).</div>
+          <div>Из последнего - участвовал в написании и запуске проекта для крупного турагентства на Laravel + VueJS, который сейчас в стадии поддержки.</div>
+          <div>В данный момент возникла необходимость перенести часть проекта на React. Хочу познать его до мелочей :)</div>
+        </div>
       </header>
     </div>
   );
