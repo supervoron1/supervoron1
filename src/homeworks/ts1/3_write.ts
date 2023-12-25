34019b4b35229653f0362624f3d70ea25ef90da8
raven <vorontsof@mail.ru>
2023-12-25 19:04:35 +0400

    task3 :fireworks:

--- src/homeworks/ts1/3_write.ts
+++ src/homeworks/ts1/3_write.ts
@@ -54,3 +54,105 @@
  * Принимает дату создания (строка)
  * */
 // export const createRandomOperation = (createdAt: string) => {};
+
+type Category = { id: string; name: string; photo?: string };
+
+type Product = {
+  id: string;
+  name: string;
+  photo: string;
+  desc?: string;
+  createdAt: string;
+  oldPrice?: number;
+  price: number;
+  category: Category;
+};
+
+type Operation = Cost | Profit;
+
+type Cost = {
+  id: string;
+  name: string;
+  desc?: string;
+  createdAt: string;
+  amount: number;
+  category: Category;
+  type: 'Cost';
+};
+
+type Profit = {
+  id: string;
+  name: string;
+  desc?: string;
+  createdAt: string;
+  amount: number;
+  category: Category;
+  type: 'Profit';
+};
+
+const generateNumber = (count: number) => Math.floor(Math.random() * count);
+
+const getRandomElement = (array: string[]): string => {
+  const randomId = generateNumber(array.length);
+  return array[randomId];
+};
+
+const products: string[] = [
+  'Кольцо из золота',
+  'Серьги из золота',
+  'Цепь из золота',
+  'Подвеска и золота',
+  'Браслет из серебра',
+  'Брошь из золота',
+];
+const productDescriptions: string[] = ['Современно', 'Эстетично', 'Представительно', 'Оригинально', 'Неповторимо'];
+const categories: string[] = ['Кольца', 'Серьги', 'Цепи', 'Подвески', 'Браслеты', 'Броши'];
+
+const photos: string[] = [
+  'https://zolotodiskont.ru/modules/photogallery/src/images/cache/199852-1030-1030-1-b504518b.webp',
+  'https://zolotodiskont.ru/modules/photogallery/src/images/cache/187843-1030-1030-1-73ad5338.webp',
+  'https://zolotodiskont.ru/modules/photogallery/src/images/cache/168696-1030-1030-1-24c6878d.webp',
+  'https://zolotodiskont.ru/modules/photogallery/src/images/cache/200140-1030-1030-1-9a0bb748.webp',
+  'https://zolotodiskont.ru/modules/photogallery/src/images/cache/199524-1030-1030-1-7e802e2b.webp',
+  'https://zolotodiskont.ru/modules/photogallery/src/images/cache/197714-1030-1030-1-0d598e32.webp',
+  'https://zolotodiskont.ru/modules/photogallery/src/images/cache/200143-1030-1030-1-83bb537c.webp',
+];
+const discountRate: number = 20;
+export const createRandomProduct = (createdAt: string): Product => {
+  const id: string = generateNumber(1000).toString();
+  const name: string = getRandomElement(products);
+  const photo: string = getRandomElement(photos);
+  const desc: string = getRandomElement(productDescriptions) + ' ' + name;
+  const oldPrice: number = generateNumber(10000);
+  const price: number = oldPrice * (1 - discountRate / 100);
+  const category: Category = {
+    id: generateNumber(100).toString(),
+    name: getRandomElement(categories),
+    photo: getRandomElement(photos),
+  };
+
+  return { id, name, photo, desc, createdAt, oldPrice, price, category };
+};
+
+const operations = ['Покупка', 'Продажа', 'Кредит', 'Рассрочка'];
+const operationDescriptions = [
+  'Приобретение товара',
+  'Продажа товара',
+  'Приобретение товара в кредит',
+  'Приобретение товара в рассрочку',
+];
+type OperationType = 'Cost' | 'Profit';
+const operationTypes: Array<OperationType> = ['Cost', 'Profit'];
+export const createRandomOperation = (createdAt: string): Operation => {
+  const id: string = generateNumber(1000).toString();
+  const name: string = getRandomElement(operations);
+  const desc: string = getRandomElement(operationDescriptions);
+  const amount: number = generateNumber(10);
+  const category: Category = {
+    id: generateNumber(1000).toString(),
+    name: getRandomElement(categories),
+  };
+  const type = operationTypes[generateNumber(operationTypes.length)];
+
+  return { id, name, desc, createdAt, amount, category, type };
+};
