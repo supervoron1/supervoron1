52935a754237bee4462057e1a12c003795ee5795
raven <vorontsof@mail.ru>
2023-12-25 14:00:36 +0400

    task1

---
+++ src/homeworks/ts1/1_base.ts
@@ -0,0 +1,76 @@
+/**
+ * Нужно превратить файл в ts и указать типы аргументов и типы возвращаемого значения
+ * */
+export const removePlus = (string: string): string => string.replace(/^\+/, '');
+
+export const addPlus = (string: string): string => `+${string}`;
+
+export const removeFirstZeros = (value: string): string => value.replace(/^(-)?[0]+(-?\d+.*)$/, '$1$2');
+
+export const getBeautifulNumber = (value: string, separator: string = ' '): string =>
+  value?.toString().replace(/\B(?=(\d{3})+(?!\d))/g, separator);
+
+export const round = (value: number, accuracy: number = 2): number => {
+  const d = 10 ** accuracy;
+  return Math.round(value * d) / d;
+};
+
+const transformRegexp: RegExp =
+  /(matrix\(-?\d+(\.\d+)?, -?\d+(\.\d+)?, -?\d+(\.\d+)?, -?\d+(\.\d+)?, )(-?\d+(\.\d+)?), (-?\d+(\.\d+)?)\)/;
+
+type TransformedCssType = {
+  x: number;
+  y: number;
+};
+export const getTransformFromCss = (transformCssString: string): TransformedCssType => {
+  const data = transformCssString.match(transformRegexp);
+  if (!data) return { x: 0, y: 0 };
+  return {
+    x: parseInt(data[6], 10),
+    y: parseInt(data[8], 10),
+  };
+};
+
+type RgbType = [number, number, number];
+export const getColorContrastValue = ([red, green, blue]: RgbType): number =>
+  // http://www.w3.org/TR/AERT#color-contrast
+  Math.round((red * 299 + green * 587 + blue * 114) / 1000);
+
+type ContrastType = 'black' | 'white';
+export const getContrastType = (contrastValue: number): ContrastType => (contrastValue > 125 ? 'black' : 'white');
+
+export const shortColorRegExp: RegExp = /^#[0-9a-f]{3}$/i;
+export const longColorRegExp: RegExp = /^#[0-9a-f]{6}$/i;
+
+export const checkColor = (color: string): void => {
+  if (!longColorRegExp.test(color) && !shortColorRegExp.test(color)) throw new Error(`invalid hex color: ${color}`);
+};
+
+export const hex2rgb = (color: string): RgbType => {
+  checkColor(color);
+  if (shortColorRegExp.test(color)) {
+    const red: number = parseInt(color.substring(1, 2), 16);
+    const green: number = parseInt(color.substring(2, 3), 16);
+    const blue: number = parseInt(color.substring(3, 4), 16);
+    return [red, green, blue];
+  }
+  const red: number = parseInt(color.substring(1, 3), 16);
+  const green: number = parseInt(color.substring(3, 5), 16);
+  const blue: number = parseInt(color.substring(5, 8), 16);
+  return [red, green, blue];
+};
+
+type ArrayItem<T> = { value: T; number: number };
+export const getNumberedArray = <T>(arr: Array<T>): ArrayItem<T>[] =>
+  arr.map((value: T, number: number): ArrayItem<T> => ({ value, number }));
+export const toStringArray = <T>(arr: ArrayItem<T>[]): string[] =>
+  arr.map(({ value, number }: ArrayItem<T>): string => `${value}_${number}`);
+
+type CustomerType = { id: number; name: string; age: number; isSubscribed: boolean };
+type TransformedCustomerType = Record<number, Omit<CustomerType, 'id'>>;
+export const transformCustomers = (customers: CustomerType[]): TransformedCustomerType => {
+  return customers.reduce((acc, customer: CustomerType) => {
+    acc[customer.id] = { name: customer.name, age: customer.age, isSubscribed: customer.isSubscribed };
+    return acc;
+  }, {} as TransformedCustomerType);
+};
